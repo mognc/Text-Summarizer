16777f82e759358592725c3ac1019912dd648a61
mognc <mogncgamer@gmail.com>
2024-11-30 21:45:09 +0500

    changed translation to summarization and added styles

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
-//const hfInference = require('@huggingface/inference-api')
-const app = express()
 
-//write api key
-//const hf = new hfInference('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')
+const app = express()
 
 const HUGGINGFACE_API_KEY = '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'
 
@@ -16,33 +13,17 @@ app.get('/', (req, res) => {
     res.render('home', { summary: 'none' })
 })
 
-// app.post('/summarize', async (req, res) => {
-//     const inputText = req.body.inputText
-
-//     try {
-//         const result = await hf.summarization({
-//             model: 't5-small',
-//             inputs: inputText,
-//             parameters: { max_length: 100 }
-//         })
-//         res.render('home', { summary: result.summary_text })
-//     } catch (error) {
-//         res.render('home', { summary: 'Error generating summary' })
-//     }
-// })
-
-
 app.post('/summarize', async (req, res) => {
     const inputText = req.body.inputText
 
     try {
         const answer = await axios.post(
-            "https://api-inference.huggingface.co/models/google-t5/t5-small",
+            "https://api-inference.huggingface.co/models/slauw87/bart_summarisation",
             { inputs: inputText },
             { headers: { Authorization: `Bearer ${HUGGINGFACE_API_KEY}` } }
         )
         console.log('API Response:', answer.data);
-        const summaryText = answer.data[0]?.translation_text || 'Summary not available';
+        const summaryText = answer.data[0]?.summary_text || 'Summary not available';
         res.render('home', { summary: summaryText })
     } catch (error) {
         res.render('home', { summary: 'error generating summary' })
